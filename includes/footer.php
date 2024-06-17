a67c18144018415ba8a5bce0f152cbc1cff4a3a2
BayInfer <148402929+BayInfer@users.noreply.github.com>
2024-06-17 17:30:40 +0700

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+
+    <footer class="py-5">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Project Pemrograman Web 2024</p>
+      </div>
+     
+    </footer>
